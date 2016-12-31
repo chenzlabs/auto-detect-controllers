8985ef55bf893d3e81cf738a85b569f4a7d090f8
machenmusik <machenmusik@gmail.com>
2016-12-30 22:04:21 -0500

    Updates

--- build.js
+++ build.js
@@ -3,7 +3,8 @@
 AFRAME.registerComponent('auto-detect-controllers', {
     schema: {
         hand: { type: 'string', default: 'right' },
-        trackedcontrols: { type: 'boolean', default: true },
+        trackedcontrols: { type: 'boolean', default: false },
+        model: { type: 'boolean', default: true },
         singlehand: { type: 'string', default: 'right' }
     },
 
@@ -41,7 +42,9 @@ AFRAME.registerComponent('auto-detect-controllers', {
         if (this.data.trackedcontrols) {
             this.el.setAttribute('tracked-controls', 'id', 'Oculus Touch ' + (this.data.hand === 'left' ? '(Left)' : '(Right)'));
             this.el.setAttribute('tracked-controls', 'controller', '0');
-        }
+        } else {
+            this.el.setAttribute(component, 'model', this.data.model);
+	}
         this.el.setAttribute(component, 'hand', this.data.hand); // although tracked-controls doesn't use this yet
         this.injectedControls = true;
     },
@@ -51,6 +54,8 @@ AFRAME.registerComponent('auto-detect-controllers', {
         if (this.data.trackedcontrols) {
             this.el.setAttribute('tracked-controls', 'id', 'OpenVR Gamepad');
             this.el.setAttribute('tracked-controls', 'controller', this.data.hand === 'left' ? '1' : '0');
+        } else {
+            this.el.setAttribute(component, 'model', this.data.model);
         }
         this.el.setAttribute(component, 'hand', this.data.hand); // although tracked-controls doesn't use this yet
         this.injectedControls = true;
